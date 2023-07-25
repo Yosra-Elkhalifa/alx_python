ae66aa23f568edfaa17815e28b1ed7ad92c01392
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-07-25 19:21:33 +0200

    Task 1 commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-new_word = "school"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:], new_word[4:]))
+print("Last 2 letters: {}".format(word[7:]))
 print("Middle word: {}".format(word[1:8]))
+word = "school"
+print("Last 2 letters: {}".format(word[4:]))
