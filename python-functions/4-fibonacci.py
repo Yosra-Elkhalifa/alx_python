ad0873e2f1984ee721a608b9f558ed1cefca23e2
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-07-28 14:29:21 +0200

    Task 4 commit

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci_sequence(n):
+    fibonacci_list = []
+    a, b = 0, 1
+
+    for _ in range(n):
+        fibonacci_list.append(a)
+        a, b = b, a + b
+
+    return fibonacci_list
