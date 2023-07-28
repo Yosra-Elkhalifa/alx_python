add30646bd77e2b6ece59aca193f118d13977f17
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-07-28 14:52:31 +0200

    Task 6 commit

---
+++ python-functions/6-password.py
@@ -0,0 +1,27 @@
+def validate_password(password):
+    # Check password length
+    if len(password) < 8:
+        return False
+
+    # Check for uppercase, lowercase, and digit
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+
+        # Check for space
+        if char.isspace():
+            return False
+
+    # Check if all conditions are met
+    if has_uppercase and has_lowercase and has_digit:
+        return True
+
+    return False
