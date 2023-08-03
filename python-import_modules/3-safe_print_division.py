44001111c943550d41c10c5594d080409a60fd55
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-03 23:18:52 +0200

    Task 3 commit

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,14 +6,17 @@ def safe_print_division(a, b):
         # Division by zero exception
     except ZeroDivisionError:
          if b == 0:
-            result = "None"
+            return None
          else:
             return result
     finally:
         print("Inside result: {}".format(result))
 
-    
-# safe_print_division(10,-2)
+    return result
+
+# a = 10
+# b = 0    
+# result = safe_print_division(a,b)
 
 
 
