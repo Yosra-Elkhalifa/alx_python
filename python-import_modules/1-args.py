b1fb2c2e3dc3928e56c29cf24c0431f539268784
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-03 21:54:12 +0200

    Task 1 commit

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,22 @@
+import sys
+
+def main():
+    argv = sys.argv[1:] #let list start from index 1 as index 0 is reserved for script name
+    num_arguments = len(argv)
+    # print number of arguments followed by : or . 
+    if num_arguments < 1:
+        print("{} arguments.".format(num_arguments))
+    elif num_arguments == 1:
+        print("{} argument.".format(num_arguments))
+    else:
+        print("{} arguments:".format(num_arguments))
+
+    # print list content
+    counter = 1
+    for arg in argv:
+        print("{} : {}".format(counter, arg))
+        counter += 1
+
+
+
+main()
