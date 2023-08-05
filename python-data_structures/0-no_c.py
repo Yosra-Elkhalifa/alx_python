49c1cfe45b23117015f9f22fd321048471a57343
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-05 22:01:40 +0200

    Task 0 commit

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -3,6 +3,10 @@ def no_c(my_string):
     for char in my_string:
         if char not in ('c', 'C'):
             new_string += char
+    return new_string
+            
+
    
-    print(new_string)
+    
+
 
