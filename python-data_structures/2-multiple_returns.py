a6dce47d41f6c0d414210c025a51196b0ccf864a
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-05 22:38:42 +0200

    Task 2 commit

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,8 @@
+sentence = ()
+def multiple_returns(sentence):
+    length_of_sentence = len(sentence)
+    first_char = sentence[0]
+    print("Length: {:d} - First character: {}".format(length_of_sentence, first_char))
+
+
+
