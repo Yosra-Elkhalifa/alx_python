1e98a020c3ef321d5e37a455c545ff86082d4eb1
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-08 23:17:16 +0200

    Task 1 commit

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,5 @@
+# Defining function that has two sets and  return a set of common elements  
+def common_elements(set_1, set_2):
+    # Similar string in both set_1 and set_2
+    result = set_1 & set_2 
+    return result
