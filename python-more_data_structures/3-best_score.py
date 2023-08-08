02fb4dc9f058a52983c694c722fc0bdd76f5a7af
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-09 00:00:45 +0200

    Task 3 commit

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -7,6 +7,4 @@ def best_score(a_dictionary):
 
 
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
+
