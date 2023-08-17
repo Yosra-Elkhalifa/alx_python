e1f29a9284129436796aa729de3373a52b41b6ed
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-18 01:07:32 +0200

    Task 0 commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,5 @@
 class Square:
-    """
-    A class that defines a square by
-    """
+    """ A class that defines a square by """
     __size = None
     def __init__(self, new_size):
         self.__size = new_size
