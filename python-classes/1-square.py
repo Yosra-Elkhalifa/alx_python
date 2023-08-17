bdf5c26d90e754e369c924bf19715e7018bdf110
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-18 01:35:39 +0200

    Task 1 commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,9 +5,9 @@ class Square:
     def __init__(self, size = 0):
         self.__size = size
         if size is not int:
-            print("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            print("size must be >= 0")
+            raise ValueError("size must be >= 0")
 
 Square.__doc__ = """ A class that defines a square by """
 
