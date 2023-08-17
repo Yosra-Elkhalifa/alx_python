5d7737a6063c054da87dfb12f45c7e19f03b73b0
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-18 01:33:46 +0200

    Task 1 commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,15 +2,11 @@
 This is module docstring for zero squre task
 """
 class Square:
-    __size = None
-    try:
-        def __init__(self, size = 0):
-            self.__size = size
-    except TypeError:
-        if __size is not int:
+    def __init__(self, size = 0):
+        self.__size = size
+        if size is not int:
             print("size must be an integer")
-    except ValueError:
-        if __size < 0:
+        if size < 0:
             print("size must be >= 0")
 
 Square.__doc__ = """ A class that defines a square by """
