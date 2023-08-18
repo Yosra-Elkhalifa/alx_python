221288908bb98b7225f8b998788b22149b6aca94
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-18 21:15:55 +0200

    Task 3 commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,17 +7,17 @@ class Square:
         """Method to instantiate an object"""
         self.__size = size
         """Getter function"""
-        @property
-        def size(self):
-            return self.__size
-        """Setter function"""
-        @size.setter
-        def size(self, value):
-            if size != int(value):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @property
+    def size(self):
+        return self.__size
+    """Setter function"""
+    @size.setter
+    def size(self, value):
+        if value != int(value):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
     def area(self):
         """Area of square function"""
         return self.__size ** 2
