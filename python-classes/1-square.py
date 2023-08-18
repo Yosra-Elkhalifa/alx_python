c7f592602bbae6f420ef783c3fcfa6f2ee8e72e1
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-18 02:09:13 +0200

    Task 1 commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,16 +4,15 @@ This is module docstring for size validation task..
 class Square:
     __size = None
     def __init__(self, size = 0):
-        self.__size = size
-        try:
-            raise TypeError
-            raise ValueError
-        except TypeError:
-            if not isinstance(int,size):
-                print("size must be an integer")
-        except ValueError:
-            if size < 0:
-                print("size must be >= 0")
+            try:
+                self.__size = size
+        
+            except TypeError:
+                if not isinstance(int,size):
+                    print("size must be an integer")
+            except ValueError:
+                if size < 0:
+                    print("size must be >= 0")
 
            
        
