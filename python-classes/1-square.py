8775d18be77f8e67fea06cb441b8bbf24272901f
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-18 19:32:28 +0200

    Task 1 commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,23 +2,25 @@
 This is module docstring for size validation task..
 """
 class Square:
+    """Class Square"""
     __size = None
     def __init__(self, size = 0):
+            """Create an instance of class Squre"""
             try:
                 self.__size = size
                 
             except TypeError:
-                if not isinstance(int,size):
+                if size != int(size):
                     print ("size must be an integer")
             except ValueError:
                 if size < 0:
                     print ("size must be >= 0")
           
 
-           
+        
        
 
-Square.__doc__ = """ A class that defines a square by """
+
 
 
 
