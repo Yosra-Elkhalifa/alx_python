6135826d21496ff6e6f776d99c321cb68fd88052
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-20 16:20:59 +0200

    Task 2 commit

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,15 @@
+"""
+A module that have a function that that returns True if the object is an instance 
+of a class that inherited (directly or indirectly) from the specified class ; 
+otherwise False.
+"""
+def inherits_from(obj, a_class):
+    """
+    A function that have two arguments: object and class 
+    returns True if the object is instance of a class
+    that inherited (directly or indirectly) from the specified class
+    """
+    if issubclass(type(obj),a_class):
+        return True
+    else: 
+        return False
