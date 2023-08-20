fa3fa4c16e71783ff40d7118ed96eb80612dad89
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-20 23:37:00 +0200

    Task 6 commit

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,51 @@
+"""
+A module that have an empty class with override to dir() method
+"""
+
+
+class MetaClass(type):
+    """
+    Override dir() method to execlude __init__subclass__
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=MetaClass):
+    """
+    BaseGeometry class that uses the overriden dir() method 
+    """
+    
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+    def area(self):
+        """
+        A public method that raise an exception
+        """
+        raise Exception ("area() is not implemented")
+    def integer_validator(self, name, value):
+        """
+        A public method that validates value:
+        - If value is not an integer: raise a TypeError exception, 
+          with the message <name> must be an integer
+        - If value is less or equal to 0: raise a ValueError exception with the 
+         message <name> must be greater than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        
+class Rectangle(BaseGeometry):
+    """
+    A class that inherits from BaseGeometry
+    """
+    __width = None
+    __height = None
+    def __init__(self, width, height):
+        """
+        A class that instantiate with width and height
+        """
+        self.__width = width
+        self.__height = height
