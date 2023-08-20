600d6fde9c8c371174f850af5bec61144082b09d
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-20 16:26:04 +0200

    Task 2 commit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -9,9 +9,7 @@ def inherits_from(obj, a_class):
     returns True if the object is instance of a class
     that inherited (directly or indirectly) from the specified class
     """
-    if issubclass(type(obj),a_class):
+    if issubclass(type(obj),a_class) and type(obj) != a_class:
         return True
-    elif type(obj) != a_class:
-         return False
     else: 
         return False
