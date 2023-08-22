f94baaaa425bb84f99e516775651435b85d96742
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-22 21:35:29 +0200

    Task 7. Update #0 commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,15 +100,15 @@ class Rectangle(Base):
         4th argument should be the x attribute
         5th argument should be the y attribute
         """
-        if len(args) < 1:
+        if len(args) >= 1:
             self.id = [0]
-        elif len(args) == 1:
+        elif len(args) >= 2:
             self.width = [1]
-        elif len(args) == 2:
+        elif len(args) >= 3:
             self.height = [2]
-        elif len(args) == 3:
+        elif len(args) >= 4:
             self.x =[3]
-        elif len(args) == 4:
+        elif len(args) >= 5:
             self.y = [4]
     
         
