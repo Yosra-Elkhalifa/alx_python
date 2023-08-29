09c696df325ad880a3a610bcee469ce786251a3a
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-29 13:48:06 +0200

    Hello route commit

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -12,7 +12,7 @@ def hello_HBNB():
     """
     A function that returns specified string when routing to /
     """
-    return " Hello HBNB!"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0",port="5000")
