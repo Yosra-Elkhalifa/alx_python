2e0f1bf8a6bb287e211abb619f34e30c66cd971b
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-31 01:27:14 +0200

    Task 0 commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 req = requests.get("https://alu-intranet.hbtn.io/status")
 req_type = type(req.text)
 req_content = req.text
-print("\t - type: {}".format(req_type))
-print("\t - content: {}".format(req_content))
+print("\t- type: {}".format(req_type))
+print("\t- content: {}".format(req_content))
