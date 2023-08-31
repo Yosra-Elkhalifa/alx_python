a82e2aca05f40d87105ac066aca7e5d4b1cd57a7
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-31 02:06:48 +0200

    Task 1 commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 """
 A script that takes in a URL, sends a request to the URL and displays 
-the value of the variable X-Request-Id in the response header
+the value of the variable X-Request-Id in the response header.
 """
 
 import requests
