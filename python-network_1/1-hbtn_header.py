8ea9435d983cc2a4b2b15eda91198460db5249e1
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-31 02:04:07 +0200

    Task 1 commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ the value of the variable X-Request-Id in the response header
 import requests
 import sys
 
-#if __name__ == "__main__":
+
 url = sys.argv[1]
 req = requests.get(url)
 x_req = req.headers.get('X-Request-Id')
