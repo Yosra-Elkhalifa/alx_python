a0d9a74cf094db63049cdf5a9525ecb55cfa68ac
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-31 02:48:55 +0200

    Task 4 commit

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,27 @@
+"""
+A Python script that takes in a letter and sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+- The letter must be sent in the variable q
+- If no argument is given, set q=""
+- If the response body is properly JSON formatted and not empty, 
+display the id and name like this: [<id>] <name>
+Otherwise:
+- Display Not a valid JSON if the JSON is invalid
+- Display No result if the JSON is empty
+"""
+
+import requests
+import sys
+
+q = sys.argv[1]
+
+if type(q) == 'str':
+    req = requests.post("http://0.0.0.0:5000/search_user")
+    if req.status_code == 204:
+        print("No result")
+    elif req.status_code == 500:
+        print("Not a valid JSON")
+    else:
+        print("[{}] {}".format(req.id, req.name))
+
