c306d0ba67054314b64d9c9fe69843d119093e7b
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-02 01:34:57 +0200

    Task 3 commit

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,12 +16,12 @@ connection = MySQLdb.connect(host="localhost", user=username,
 cursor = connection.cursor()
 
 query = "SELECT * FROM states \
-            WHERE name COLLATE utf8mb4_bin = '%s' "
+            WHERE name COLLATE utf8mb4_bin = %s ", (state_name_searched,)
 
 #To be safe from SQL injection passing state name as tuple and 
 # it has to be followed by comma 
 
-cursor.execute(query, (state_name_searched,))
+cursor.execute(query)
 
 States = cursor.fetchall()
 
