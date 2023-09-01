709d228439fe68a14005d7e3e1e713cab63e16c3
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-01 21:26:09 +0200

    filter states commit

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+"""
+This a module for Object Relational Mapping project 
+which start with task 0  that has select statment
+"""
+import sys
+import MySQLdb
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+
+connection = MySQLdb.connect(host="localhost", user = username, passwd = password, db = database )
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM states WHERE name like 'N%' ")
+
+States= cursor.fetchall() 
+
+for state in States:
+    print(state)
