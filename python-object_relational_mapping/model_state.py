10f49273ecdbe0ce345cc80dc482a12cc5b43d9b
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-02 23:21:49 +0200

    updating class name

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -22,15 +22,19 @@ connection = database.connect()
 
 Base = declarative_base()
 
-class States(Base):
-    '''
-    A class that inherits from Base 
-    '''
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True,nullable=False)
-    name = Column(String(128),nullable=False)
-
-    def __init__(self, name):
+class State(Base):
+   """
+   State class:
+    - inherits from Base 
+    - links to the MySQL table states
+    - class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
+    - class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+   """
+   __tablename__ = "states"
+   id = Column(Integer, primary_key=True, autoincrement=True,nullable=False)
+   name = Column(String(128),nullable=False)
+   
+   def __init__(self, name):
         self.name = name
 
 Base.metadata.create_all(bind-database)
