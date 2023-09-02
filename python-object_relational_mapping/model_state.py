0acf4231d84b7ecb524e8a4502a0b334855ff7c5
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-02 13:57:41 +0200

    using doc()

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -23,17 +23,14 @@ connection = database.connect()
 Base = declarative_base()
 
 class States(Base):
-    """
-    States class that inherits from Base:
-     links to the MySQL table states
-    - class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-    - class attribute name that represents a column of a string with maximum 128 characters and can’t be null
-    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True,nullable=False)
     name = Column(String(128),nullable=False)
 
     def __init__(self, name):
         self.name = name
+        
+States.__doc__ = """ A class that inherits from Base"""
 
 Base.metadata.create_all(bind-database)
+
