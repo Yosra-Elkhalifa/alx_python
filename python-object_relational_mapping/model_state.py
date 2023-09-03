1073a9413ada2eb4ade79aa9e8a77fb802ee0a77
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-04 01:20:50 +0200

    PEP 8 validation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,18 +5,17 @@ Base = declarative_base()
 
 
 class State(Base):
-   """
-   State class:
+    """
+    State class:
 
-   Attributes:
-   - id: of type integar and constitute the primary key 
+    Attributes:
+    - id: of type integar and constitute the primary key
    - name: of type string and represents the name of state
    Args:
    - Base : instance of declarative_base
    """
-   __tablename__ = "states"
-   id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-   name = Column(String(128), nullable=False)
-   
-   def __init__(self, name):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    def __init__(self, name):
         self.name = name
